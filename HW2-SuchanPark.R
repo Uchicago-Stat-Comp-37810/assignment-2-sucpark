dd40f21587757ea6d5ea1dfdf3ae46d17dc91d98
sucpark <32281524+sucpark@users.noreply.github.com>
2017-10-11 20:24:57 -0500

    Change 3
    Make a function of graphing portion.

--- HW2-SuchanPark.R
+++ HW2-SuchanPark.R
@@ -93,24 +93,26 @@ burnIn = 5000 # Discard 5000 data when computing acceptance.
 acceptance = 1-mean(duplicated(chain[-(1:burnIn),])) #Compute the rejection rate of proposal function
 
 ### Summary: #######################
-
-par(mfrow = c(2,3))
-hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of a", xlab="True value = red line" )
-abline(v = mean(chain[-(1:burnIn),1]))
-abline(v = trueA, col="red" )
-hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of b", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),2]))
-abline(v = trueB, col="red" )
-hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of sd", xlab="True value = red line")
-abline(v = mean(chain[-(1:burnIn),3]) )
-abline(v = trueSd, col="red" )
-
-plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a")
-abline(h = trueA, col="red" )
-plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b")
-abline(h = trueB, col="red" )
-plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd")
-abline(h = trueSd, col="red" )
-
+Summary<-function(chain, burnIn,trueA,trueB,trueSd)
+{
+  par(mfrow = c(2,3))
+  hist(chain[-(1:burnIn),1],nclass=30, main="Posterior of a", xlab="True value = red line" )
+  abline(v = mean(chain[-(1:burnIn),1]))
+  abline(v = trueA, col="red" )
+  hist(chain[-(1:burnIn),2],nclass=30, main="Posterior of b", xlab="True value = red line")
+  abline(v = mean(chain[-(1:burnIn),2]))
+  abline(v = trueB, col="red" )
+  hist(chain[-(1:burnIn),3],nclass=30, main="Posterior of sd", xlab="True value = red line")
+  abline(v = mean(chain[-(1:burnIn),3]) )
+  abline(v = trueSd, col="red" )
+  
+  plot(chain[-(1:burnIn),1], type = "l", xlab="True value = red line" , main = "Chain values of a")
+  abline(h = trueA, col="red" )
+  plot(chain[-(1:burnIn),2], type = "l", xlab="True value = red line" , main = "Chain values of b")
+  abline(h = trueB, col="red" )
+  plot(chain[-(1:burnIn),3], type = "l", xlab="True value = red line" , main = "Chain values of sd")
+  abline(h = trueSd, col="red" )
+}
+Summary(chain, burnIn,trueA,trueB,trueSd)
 # for comparison:
 summary(lm(y~x))
